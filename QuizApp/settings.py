0efc785724939dc83f528459de51a5a379636de8
sadam-hussain-s <sadam89391@gmail.com>
2020-12-22 21:36:17 +0530

    online quiz

--- QuizApp/settings.py
+++ QuizApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'iw%!+mw70or882v=9s^q&buo0*dy=#839wcmm)wo=s_tpxl!i#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','django-online-quiz.herokuapp.com']
 
@@ -123,6 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'statics'),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
